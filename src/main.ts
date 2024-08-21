60396d37b5afb6811d9b9c257f456d861633e249
Aloysius Gilang <gilangpramdya84@gmail.com>
2024-08-21 19:45:24 +0700

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('BlogPost REST API')
